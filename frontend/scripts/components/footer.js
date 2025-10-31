9ab4342e5bfbaf866bfd753c2c48f29f6f596ade
EduVemba <eduvemba18@gmail.com>
2025-10-31 11:28:32 +0000

    feat: basic styling.

--- frontend/scripts/components/footer.js
+++ frontend/scripts/components/footer.js
@@ -2,5 +2,6 @@ export function createFooter() {
     const footer = document.createElement('footer');
     //TODO: Adicionar conteudo no rodapé (links, informações de contato, etc.)
     footer.className = 'site-footer';
+    footer.textContent = "© 2024 Orchids Inc. || Eduardo Vemba & João Freitas.";
     return footer;
 }
--- frontend/scripts/components/header.js
+++ frontend/scripts/components/header.js
@@ -3,14 +3,8 @@
 export function createHeader() {
     const header = document.createElement('header');
     //TODO: Adicionar conteudo no cabeçalho (menu, logo, etc.)
-    header.textContent = 'Cabeçalho do Site';
+    header.textContent = 'The Orchids Site';
     header.className = 'site-header';
-
-    header.style.backgroundColor = '#4CAF50';
-    header.style.color = 'white';
-    header.style.padding = '20px';
-    header.style.textAlign = 'center';
-    header.style.fontSize = '24px';
 
     return header;
 }

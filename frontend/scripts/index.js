581404811adeae48db72d90eb8b60fd0a9179074
EduVemba <eduvemba18@gmail.com>
2025-10-31 11:07:41 +0000

    feat: header de teste para criação de elementos com JS

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,1 +1,12 @@
 //TODO: ficheiro principal que vai gerenciar a aplicação
+import { createHeader } from './components/header.js';
+import { createFooter } from './components/footer.js';
+
+
+const body = document.body;
+
+const header = createHeader();
+const footer = createFooter();
+
+body.appendChild(header);
+body.appendChild(footer);

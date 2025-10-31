af2c9a1c48f60d442d318be6b9ac5e2640e7f12d
EduVemba <eduvemba18@gmail.com>
2025-10-31 13:31:49 +0000

    feat: logo

--- frontend/scripts/components/header.js
+++ frontend/scripts/components/header.js
@@ -2,10 +2,18 @@ import { createMenuButton } from './menu.js';
 
 export function createHeader() {
     const header = document.createElement('header');
-    //TODO: Adicionar conteudo no cabeçalho (menu, logo, etc.)
+    const logo = document.createElement('img');
+    logo.src = './images/logo/image.png';
+    logo.className = 'site-logo';
+    logo.alt = 'Orchids Logo';
+    
     const title = document.createElement('div');
-    title.textContent = 'The Orchids Site';
+    const titleText = document.createElement('span');
+    titleText.textContent = 'The Orchids Site';
     title.className = 'site-title';
+    
+    title.appendChild(logo);
+    title.appendChild(titleText);
     
     header.className = 'site-header';
     header.appendChild(title);

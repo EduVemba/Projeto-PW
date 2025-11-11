39f5e90208e5135d8975aa2da808733ea9080628
joaofreitas05 <2024151451@estudantes.ips.pt>
2025-11-11 22:31:29 +0000

    Commit das classes orchids e orchidsCollection

--- www/scripts/components/footer.js
+++ www/scripts/components/footer.js
@@ -2,6 +2,6 @@ export function createFooter() {
     const footer = document.createElement('footer');
     //TODO: Adicionar conteudo no rodapé (links, informações de contato, etc.)
     footer.className = 'site-footer';
-    footer.textContent = "© 2024 Orchids Inc. || Eduardo Vemba & João Freitas.";
+    footer.textContent = "© 2025 Orchids Inc. || Eduardo Vemba & João Freitas.";
     return footer;
 }

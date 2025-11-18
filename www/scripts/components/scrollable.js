d72d4a49f34ff7caf38d37989adf06d715ebdef3
EduVemba <eduvemba18@gmail.com>
2025-11-18 10:36:34 +0000

    refactor: some changes.

--- www/scripts/components/scrollable.js
+++ www/scripts/components/scrollable.js
@@ -4,11 +4,16 @@
 // no card fazer on click para fazer 
 export const scrollabeDiv = (header, type) => {
     const page = document.createElement('div');
-    page.classList.add('scrollable-container')
+    page.classList.add('scrollable-container');
+
+    const createBtn = document.createElement('span');
+    createBtn.textContent = "+ Create Orchid";
+    createBtn.classList.add('create-btn');
 
     const inner = document.createElement('li');
     inner.classList.add('scrollable-content');
 
+    page.appendChild(createBtn);
     page.appendChild(inner);
 
     window.location.hash = `#card-${encodeURIComponent(header)}`;

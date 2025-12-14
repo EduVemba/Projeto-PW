976a0e7b0c57b9345da903d9684ec0a931fedee1
EduVemba <eduvemba18@gmail.com>
2025-12-14 10:02:42 +0000

    servidor a correr

--- servidor/server/server.js
+++ servidor/server/server.js
@@ -2,4 +2,18 @@
 
 /**
  * @brief Ficheiro de servidor para fazer Network rotting.
- */
+*/
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+app.get('/', (req, res) => {
+  res.send('Esse é o Projeto!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
